9e1ef5c198c8e151242a826f08a32d183b4c8a6d
jakew12354 <jakedylanwiltshire@gmail.com>
2022-06-07 13:38:24 -0400

    there you go neal this is what alt+shift+F does

--- Jakebot.java
+++ Jakebot.java
@@ -1,4 +1,5 @@
 package jakebot;
+
 import robocode.*;
 import static robocode.util.Utils.normalRelativeAngleDegrees;
 
@@ -7,108 +8,109 @@ import static robocode.util.Utils.normalRelativeAngleDegrees;
 /**
  * Jakebot - a robot by (your name here)
  */
-public class Jakebot extends AdvancedRobot
-{
-     int count = 0;
-    double gunTurnAmt;
-    String trackName;
+public class Jakebot extends AdvancedRobot {
+	int count = 0;
+	double gunTurnAmt;
+	String trackName;
+
 	/**
 	 * run: Jakebot's default behavior
 	 */
 	public void run() {
-	trackName = null;
-    setAdjustGunForRobotTurn(true);
-    gunTurnAmt = 5;
-		while(true) {
+		trackName = null;
+		setAdjustGunForRobotTurn(true);
+		gunTurnAmt = 5;
+		while (true) {
 			setTurnGunRight(gunTurnAmt);
-            count++;
-            if (count > 1) {
-                gunTurnAmt = -20;
-            }
-            if (count > 2) {
-                gunTurnAmt = 40;
-            }
+			count++;
+			if (count > 1) {
+				gunTurnAmt = -20;
+			}
+			if (count > 2) {
+				gunTurnAmt = 40;
+			}
 			if (count > 4) {
 				trackName = null;
 			}
-		execute();
+			execute();
 		}
 	}
 
-	
 	public void onScannedRobot(ScannedRobotEvent e) {
 		if (trackName != null && !e.getName().equals(trackName)) {
-            return;
-        }
-        
-        if (trackName == null) {
-            trackName = e.getName();
-            out.println("Tracking " + trackName);
-        }
+			return;
+		}
+
+		if (trackName == null) {
+			trackName = e.getName();
+			out.println("Tracking " + trackName);
+		}
 		count = 0;
 		if (e.getDistance() > 100) {
 			gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
-		setTurnGunRight(gunTurnAmt);
-		setTurnRight(e.getBearing());
-		setAhead(e.getDistance() - 90);
-		return;
+			setTurnGunRight(gunTurnAmt);
+			setTurnRight(e.getBearing());
+			setAhead(e.getDistance() - 90);
+			return;
 		}
-			gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
+		gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
 		setTurnGunRight(gunTurnAmt);
 		setFireBullet(3);
 		if (e.getDistance() < 50) {
-				if (e.getBearing() > -50 && e.getBearing() <=50) {
-					setAhead(-10);
-				} else {
+			if (e.getBearing() > -50 && e.getBearing() <= 50) {
+				setAhead(-10);
+			} else {
 				setAhead(10);
 				setFireBullet(3);
-				}
 			}
-			scan();
+		}
+		scan();
 	}
 
-			public void onHitRobot(HitRobotEvent e) {
-			if (trackName != null && !trackName.equals(e.getName())) {
+	public void onHitRobot(HitRobotEvent e) {
+		if (trackName != null && !trackName.equals(e.getName())) {
 			out.println("Tracking " + e.getName() + " due to collision");
-			}
-			trackName = e.getName();
-			gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
-			setTurnGunRight(gunTurnAmt);
-			setFireBullet(2);
-			setAhead(-50);
 		}
+		trackName = e.getName();
+		gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
+		setTurnGunRight(gunTurnAmt);
+		setFireBullet(2);
+		setAhead(-50);
+	}
 
 	/**
 	 * onHitByBullet: What to do when you're hit by a bullet
 	 */
-		public void onHitByBullet(HitByBulletEvent e) {
+	public void onHitByBullet(HitByBulletEvent e) {
 		if (trackName != null && !trackName.equals(e.getName())) {
 			out.println("Tracking " + e.getName() + " due to being shot");
-		}	
+		}
 		trackName = e.getName();
 		gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
 		setTurnGunRight(gunTurnAmt);
-		execute();	
-		scan();	
+		execute();
+		scan();
 	}
-	
+
 	/**
 	 * onHitWall: What to do when you hit a wall
 	 */
-		public void onHitWall(HitWallEvent e) {
-		e.getBearing(); 
+	public void onHitWall(HitWallEvent e) {
+		e.getBearing();
 		if (e.getBearing() < 90) {
 			setTurnRight(90);
 			setAhead(50);
-		} else{
+		} else {
 			setTurnLeft(90);
 			setAhead(50);
-			}
-		count = 0;
-	}	
-		public void onBulletmissed(BulletMissedEvent e) {
+		}
 		count = 0;
 	}
+
+	public void onBulletmissed(BulletMissedEvent e) {
+		count = 0;
+	}
+
 	public void onWin(WinEvent e) {
 		setAhead(10000);
 		setTurnLeft(90);
